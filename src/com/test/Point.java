181269dfbdcd735bf61798e591efc178cf09f872
Swati Jain <swatibpv@yahoo.com>
2014-06-20 13:32:23 +0530

    Commit for Test regarding Questions 2nd and 3rd.

---
+++ src/com/test/Point.java
@@ -0,0 +1,6 @@
+package com.test;
+
+public class Point {
+
+	int x,y;
+}
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,9 +1,43 @@
 package com.test;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Test {
-	
+
 	public static void main(String[] args) {
-		System.out.println("First Git Project");
+
+		System.out.println(convertColorToHexadecimal(68, 58, 197));
+
+		calculatePercentile();
+	}
+
+	public static void calculatePercentile() {
+		Integer[] array = { 8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2 };
+		int arrayLen = array.length;
+		double percentile = 0;
+		System.out.println("Array Length: " + arrayLen);
+		Set<Integer> uniqueSet = new TreeSet<Integer>();
+		Collections.addAll(uniqueSet, array);
+
+		for (Integer integer : uniqueSet) {
+			double count = 0;
+			for (int i = 0; i < arrayLen; i++) {
+				if (integer == array[i])
+					count++;
+			}
+			percentile += count * 100 / arrayLen;
+			System.out.println(Math.round(percentile)
+					+ "% of the numbers are less than or equal to " + integer);
+		}
+
+	}
+
+	public static String convertColorToHexadecimal(int r, int g, int b) {
+		String hexCode = "#" + Integer.toHexString(r) + Integer.toHexString(g)
+				+ Integer.toHexString(b);
+		return hexCode;
 	}
 
 }
